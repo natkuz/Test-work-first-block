26b343a9b574a4a2d1fa0bb72fb1d82c66664131
NatalyaKuznetsova <nv8282@yandex.ru>
2022-12-02 22:28:25 +0800

    Completed method for fill array

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string[] FillArray(int amountElements)
+{
+    string[] array = new string[amountElements];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Enter element {i + 1} of {amountElements}:");
+        array[i] = Console.ReadLine()!;
+    }
+    return array;
+}
